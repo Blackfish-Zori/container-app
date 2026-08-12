c582590dfed2e7be389fb918314c3a8fdd68c2d8
Zori <145198002+Blackfish-Zori@users.noreply.github.com>
2026-08-12 18:56:33 +0200

    Od 12.8.

---
+++ front/scripts/functions.js
@@ -0,0 +1,11 @@
+import { dummyTaskData } from "./constants.js";
+
+export function getTasks() {
+  return dummyTaskData;
+}
+
+export function addTask() {}
+
+export function checkTask() {}
+
+export function deleteTask() {}
